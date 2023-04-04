9d88f786bba683510ae9644fb01c37d0f2023be6
Chndrafajar <chandrafajar356@gmail.com>
2023-04-03 23:12:02 -0700

    update db

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ const flash = require('connect-flash');
 
 //import mongoose
 const mongoose = require('mongoose');
-mongoose.connect('mongodb+srv://yukfood:apiyukfood@cluster0.lwalar5.mongodb.net/db_yukfood?retryWrites=true&w=majority', { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true, useFindAndModify: false });
+mongoose.connect('mongodb://127.0.0.1:27017/db_yukfood', { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true, useFindAndModify: false });
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
